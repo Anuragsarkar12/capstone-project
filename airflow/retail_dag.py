b86962d5b19631dd39c19a07c9d28e4f981a979c
Avranil08 <avranilset@gmail.com>
2026-04-19 14:33:35 +0000

    feat : Making the code modular

--- airflow/retail_dag.py
+++ airflow/retail_dag.py
@@ -5,6 +5,8 @@ from airflow.providers.databricks.operators.databricks import DatabricksSubmitRu
 
 
 
+# Loading Config file
+
 CONFIG_PATH = "/opt/airflow/dags/config/config.yaml"
 
 with open(CONFIG_PATH, "r") as f:
@@ -12,15 +14,14 @@ with open(CONFIG_PATH, "r") as f:
 
 airflow_cfg = config["airflow"]
 
-
-
 DATABRICKS_CONN_ID = airflow_cfg["databricks"]["conn_id"]
 CLUSTER_ID = airflow_cfg["databricks"]["cluster_id"]
 NOTEBOOK_PATHS = airflow_cfg["notebooks"]
-
 DAG_CONFIG = airflow_cfg["dag"]
 
 
+
+# Loading Default Arguements
 
 default_args = {
     "owner": DAG_CONFIG["owner"],
@@ -32,6 +33,25 @@ default_args = {
 
 
 
+# Function to create Task
+
+def create_databricks_task(task_id, notebook_path, params=None):
+    return DatabricksSubmitRunOperator(
+        task_id=task_id,
+        databricks_conn_id=DATABRICKS_CONN_ID,
+        json={
+            "existing_cluster_id": CLUSTER_ID,
+            "notebook_task": {
+                "notebook_path": notebook_path,
+                "base_parameters": params or {}
+            }
+        }
+    )
+
+
+
+# DAG Definition
+
 with DAG(
     dag_id=DAG_CONFIG["dag_id"],
     default_args=default_args,
@@ -41,78 +61,42 @@ with DAG(
     tags=DAG_CONFIG["tags"],
 ) as dag:
 
-    ingest_raw = DatabricksSubmitRunOperator(
-        task_id="ingest_raw",
-        databricks_conn_id=DATABRICKS_CONN_ID,
-        json={
-            "existing_cluster_id": CLUSTER_ID,
-            "notebook_task": {
-                "notebook_path": NOTEBOOK_PATHS["bronze"]
-            }
-        }
-    )
-
-    build_silver = DatabricksSubmitRunOperator(
-        task_id="build_silver",
-        databricks_conn_id=DATABRICKS_CONN_ID,
-        json={
-            "existing_cluster_id": CLUSTER_ID,
-            "notebook_task": {
-                "notebook_path": NOTEBOOK_PATHS["silver"]
-            }
-        }
-    )
-
-    dq_check_silver = DatabricksSubmitRunOperator(
-        task_id="dq_check_silver",
-        databricks_conn_id=DATABRICKS_CONN_ID,
-        json={
-            "existing_cluster_id": CLUSTER_ID,
-            "notebook_task": {
-                "notebook_path": NOTEBOOK_PATHS["dq"],
-                "base_parameters": {
-                    "layer": airflow_cfg["dq_layers"]["silver"]
-                }
-            }
-        }
-    )
-
-    build_gold = DatabricksSubmitRunOperator(
-        task_id="build_gold",
-        databricks_conn_id=DATABRICKS_CONN_ID,
-        json={
-            "existing_cluster_id": CLUSTER_ID,
-            "notebook_task": {
-                "notebook_path": NOTEBOOK_PATHS["gold"]
-            }
-        }
-    )
-
-    dq_check_gold = DatabricksSubmitRunOperator(
-        task_id="dq_check_gold",
-        databricks_conn_id=DATABRICKS_CONN_ID,
-        json={
-            "existing_cluster_id": CLUSTER_ID,
-            "notebook_task": {
-                "notebook_path": NOTEBOOK_PATHS["dq"],
-                "base_parameters": {
-                    "layer": airflow_cfg["dq_layers"]["gold"]
-                }
-            }
-        }
-    )
-
-    load_snowflake = DatabricksSubmitRunOperator(
-        task_id="load_snowflake",
-        databricks_conn_id=DATABRICKS_CONN_ID,
-        json={
-            "existing_cluster_id": CLUSTER_ID,
-            "notebook_task": {
-                "notebook_path": NOTEBOOK_PATHS["snowflake"]
-            }
-        }
-    )
-
+    # Tasks
    
+    ingest_raw = create_databricks_task(
+        task_id="ingest_raw",
+        notebook_path=NOTEBOOK_PATHS["bronze"]
+    )
 
+    build_silver = create_databricks_task(
+        task_id="build_silver",
+        notebook_path=NOTEBOOK_PATHS["silver"]
+    )
+
+    dq_check_silver = create_databricks_task(
+        task_id="dq_check_silver",
+        notebook_path=NOTEBOOK_PATHS["dq"],
+        params={"layer": airflow_cfg["dq_layers"]["silver"]}
+    )
+
+    build_gold = create_databricks_task(
+        task_id="build_gold",
+        notebook_path=NOTEBOOK_PATHS["gold"]
+    )
+
+    dq_check_gold = create_databricks_task(
+        task_id="dq_check_gold",
+        notebook_path=NOTEBOOK_PATHS["dq"],
+        params={"layer": airflow_cfg["dq_layers"]["gold"]}
+    )
+
+    load_snowflake = create_databricks_task(
+        task_id="load_snowflake",
+        notebook_path=NOTEBOOK_PATHS["snowflake"]
+    )
+
+
+  
+    # DAG Flow
+ 
     ingest_raw >> build_silver >> dq_check_silver >> build_gold >> dq_check_gold >> load_snowflake
